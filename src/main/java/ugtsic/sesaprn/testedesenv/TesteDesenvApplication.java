f970b8fc26ca08324fb12cb0df91e8bad854802c
Ana Camilly <camillyguedes2@gmail.com>
2023-12-15 09:37:15 -0300

    init

---
+++ src/main/java/ugtsic/sesaprn/testedesenv/TesteDesenvApplication.java
@@ -0,0 +1,13 @@
+package ugtsic.sesaprn.testedesenv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteDesenvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TesteDesenvApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ugtsic/sesaprn/testedesenv/TesteDesenvApplicationTests.java
@@ -0,0 +1,13 @@
+package ugtsic.sesaprn.testedesenv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteDesenvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
